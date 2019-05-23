a5bed432d3e1e6e00b564a7575503abd417f0189
Gwena <gwena.cs@gmail.com>
2019-05-23 17:54:55 +0900

    Update autoencoder

--- models/stacked_debert_dae/autoencoder.py
+++ models/stacked_debert_dae/autoencoder.py
@@ -9,20 +9,24 @@ class AutoEncoder(nn.Module):
         self.encoder_layer1 = nn.Sequential(
             nn.Linear(768, 256), nn.ReLU(),
             nn.Linear(256, 128), nn.ReLU(),
-            nn.Linear(128, 64), nn.ReLU(),
         )
         self.encoder_layer2 = nn.Sequential(
+            nn.Linear(128, 64), nn.ReLU(),
             nn.Linear(64, 32), nn.ReLU(),
+        )
+        self.encoder_layer3 = nn.Sequential(
             nn.Linear(32, 24), nn.ReLU(),
             nn.Linear(24, 12)
         )
         self.decoder_layer1 = nn.Sequential(
             nn.Linear(12, 24), nn.ReLU(),
             nn.Linear(24, 32), nn.ReLU(),
-            nn.Linear(32, 64), nn.ReLU(),
         )
         self.decoder_layer2 = nn.Sequential(
+            nn.Linear(32, 64), nn.ReLU(),
             nn.Linear(64, 128), nn.ReLU(),
+        )
+        self.decoder_layer3 = nn.Sequential(
             nn.Linear(128, 256), nn.ReLU(),
             nn.Linear(256, 768), nn.Tanh()
         )
@@ -31,9 +35,11 @@ class AutoEncoder(nn.Module):
         out_encoder = self.encoder_layer1(x)
         # print(np.shape(out_encoder))
         out_encoder = self.encoder_layer2(out_encoder)
+        out_encoder = self.encoder_layer3(out_encoder)
         # print(np.shape(out_encoder))
         out_decoder = self.decoder_layer1(out_encoder)
         # print(np.shape(out_decoder))
         out_decoder = self.decoder_layer2(out_decoder)
+        out_decoder = self.decoder_layer3(out_decoder)
         # print(np.shape(out_decoder))
         return out_encoder, out_decoder
