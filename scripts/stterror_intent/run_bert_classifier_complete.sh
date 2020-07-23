365afe79723a31d5d7591b12b946ec312acfe3e9
gwena <gwena.cs@gmail.com>
2020-07-23 15:05:48 +0900

    Fix script: --do_train followed by --do_eval

--- scripts/stterror_intent/run_bert_classifier_complete.sh
+++ scripts/stterror_intent/run_bert_classifier_complete.sh
@@ -15,7 +15,7 @@ for DATASET in chatbot; do
             OUT_PATH="${OUTPUT_DIR}/${DATASET}_ep${EPOCH}_bs${BS_TRAIN}_complete_seed${SEED}/"
 
             # Train
-            CUDA_VISIBLE_DEVICES=0,1,2 python ../../run_classifier.py --seed $SEED --task_name "${DATASET}_intent" --save_best_model --do_eval --do_lower_case --data_dir $DATA_DIR --bert_model bert-base-uncased --max_seq_length 128 --train_batch_size $BS_TRAIN --eval_batch_size $BS_EVAL --learning_rate 2e-5 --num_train_epochs $EPOCH --output_dir $OUT_PATH
+            CUDA_VISIBLE_DEVICES=0,1,2 python ../../run_classifier.py --seed $SEED --task_name "${DATASET}_intent" --save_best_model --do_train --do_lower_case --data_dir $DATA_DIR --bert_model bert-base-uncased --max_seq_length 128 --train_batch_size $BS_TRAIN --eval_batch_size $BS_EVAL --learning_rate 2e-5 --num_train_epochs $EPOCH --output_dir $OUT_PATH
             # Eval
             CUDA_VISIBLE_DEVICES=0,1,2 python ../../run_classifier.py --seed $SEED --task_name "${DATASET}_intent" --save_best_model --do_eval --do_lower_case --data_dir $DATA_DIR --bert_model bert-base-uncased --max_seq_length 128 --train_batch_size $BS_TRAIN --eval_batch_size $BS_EVAL --learning_rate 2e-5 --num_train_epochs $EPOCH --output_dir $OUT_PATH
         done
