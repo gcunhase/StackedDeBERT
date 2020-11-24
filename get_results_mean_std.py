962b2dfaff94bb6388e7370ab9e4ff54f443ac41
gwena <gwena.cs@gmail.com>
2020-11-24 14:55:13 +0900

    Get results table: mean/std

---
+++ get_results_mean_std.py
@@ -0,0 +1,42 @@
+import json
+import numpy as np
+
+
+# STTError
+# Parameters:
+#   is_incomplete_test: True if model was trained with complete data and tested with incomplete
+
+root_name = './results_thesis/results_stacked_debert_dae_earlyStopWithLoss_lower_STTerror/'
+#root_name = './results_thesis/results_stacked_debert_dae_complete_earlyStopWithEvalLoss/'
+stt_error, dataname, epoch, epae, lrae, bs, tts_stt_type = [True, "chatbot", 100, 100, '0.00001', 8, 'macsay_witai']
+
+prefix = ''
+if stt_error:
+    prefix = tts_stt_type + '/'
+
+if float(lrae) < 0.001:
+    root_dir = '{root_name}/{dataname}/{prefix}bs{bs}_epae{epae}_lrae{lrae}/{dataname}_ep{ep}_bs{bs}_'.\
+        format(root_name=root_name, dataname=dataname, prefix=prefix, bs=bs, epae=epae, ep=epoch, lrae=lrae)
+else:
+    root_dir = '{root_name}/{dataname}/{prefix}bs{bs}_epae{epae}/{dataname}_ep{ep}_bs{bs}_'. \
+        format(root_name=root_name, dataname=dataname, prefix=prefix, bs=bs, epae=epae, ep=epoch)
+
+f1_micro_str_all = ""
+f1_micro_arr = []
+f1_micro_str_all += "| {}    ".format(0)
+for i in range(1, 10 + 1):
+    #tmp_dir = "{}seed{}/".format(root_dir, i)
+    tmp_dir = "{}seed{}_second_layer_epae{}/".format(root_dir, i, epae)
+    tmp_dir += "eval_results_test.json"
+    #tmp_dir += "eval_results.json"
+
+    # Load json file
+    with open(tmp_dir, 'r') as f:
+        datastore = json.load(f)
+        f1_score = datastore['f1_micro']
+        f1_micro_arr.append(f1_score)
+        f1_micro_str_all += "|{:.2f}".format(f1_score*100)
+
+f1_micro_str_all += "|{:.2f}|{:.2f}|\n".format(np.mean(f1_micro_arr)*100, np.std(f1_micro_arr)*100)
+
+print(f1_micro_str_all)
---
+++ get_results_mean_std_testWithIncompleteData.py
@@ -0,0 +1,34 @@
+import json
+import numpy as np
+
+# Use this script if the model was trained with complete data but you wish to test it with another set of test data,
+#  in our case incomplete data.
+
+root_name = './results_thesis/results_stacked_debert_dae_complete_earlyStopWithEvalLoss/test_with_incomplete/'
+
+dataname, epoch, epae, lrae, bs, tts_stt_type = ["chatbot", 3, 1000, 0.001, 8, 'macsay_witai']
+
+if lrae < 0.001:
+    root_dir = '{root_name}/{dataname}/bs{bs}_epae{epae}_lrae{lrae}/{dataname}_ep{ep}_bs{bs}_'.\
+        format(root_name=root_name, dataname=dataname, bs=bs, epae=epae, ep=epoch, lrae=lrae)
+else:
+    root_dir = '{root_name}/{dataname}/bs{bs}_epae{epae}/{dataname}_ep{ep}_bs{bs}_'. \
+        format(root_name=root_name, dataname=dataname, bs=bs, epae=epae, ep=epoch)
+
+f1_micro_str_all = ""
+f1_micro_arr = []
+f1_micro_str_all += "| {}    ".format(0)
+for i in range(1, 10 + 1):
+    tmp_dir = "{}seed{}_epae{}/".format(root_dir, i, epae)
+    tmp_dir += "eval_results_test_{tts_stt_type}.json".format(tts_stt_type=tts_stt_type)
+
+    # Load json file
+    with open(tmp_dir, 'r') as f:
+        datastore = json.load(f)
+        f1_score = datastore['f1_micro']
+        f1_micro_arr.append(f1_score)
+        f1_micro_str_all += "|{:.2f}".format(f1_score*100)
+
+f1_micro_str_all += "|{:.2f}|{:.2f}|\n".format(np.mean(f1_micro_arr)*100, np.std(f1_micro_arr)*100)
+
+print(f1_micro_str_all)
